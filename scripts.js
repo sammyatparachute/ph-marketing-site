6633e3ff472a105347599bd7cbcf013650ba16c9
sammygallo <sammygallo@gmail.com>
2022-08-30 11:11:27 -0400

    Made setSupplierName function

--- scripts.js
+++ scripts.js
@@ -1,21 +1,29 @@
-function typeformPopup(typeformURL,webinarSlot,request_type,supplier)
-  {
-    var reference = typeformEmbed.makePopup(
-      typeformURL+"?"+"webinarslot="+webinarSlot+"&request_type="+request_type+"&supplier="+supplier,
-      { mode: "popup", autoClose: 5, hideHeaders: true, hideFooters: true }
-    );
-    reference.open();
-  }
-  
-  var typeformURL = "https://parachutehealthdme.typeform.com/to/hUhvu4QC";
-  
-  timeSlot1 = "11:00am";
-  timeSlot2 = "11:30am";
-  timeSlot3 = "12:00pm";
-  timeSlot4 = "12:30pm";
-  timeSlot5 = "2:00pm";
-  timeSlot6 = "2:30pm";
-  timeSlot7 = "3:00pm";
-  timeSlot8 = "3:30pm";
+function typeformPopup(typeformURL, webinarSlot, request_type, supplier) {
+  var reference = typeformEmbed.makePopup(
+    typeformURL +
+      "?" +
+      "webinarslot=" +
+      webinarSlot +
+      "&request_type=" +
+      request_type +
+      "&supplier=" +
+      supplier,
+    { mode: "popup", autoClose: 5, hideHeaders: true, hideFooters: true }
+  );
+  reference.open();
+}
 
-  document.getElementById("supplier-name").textContent = supplier_name
+var typeformURL = "https://parachutehealthdme.typeform.com/to/hUhvu4QC";
+
+timeSlot1 = "11:00am";
+timeSlot2 = "11:30am";
+timeSlot3 = "12:00pm";
+timeSlot4 = "12:30pm";
+timeSlot5 = "2:00pm";
+timeSlot6 = "2:30pm";
+timeSlot7 = "3:00pm";
+timeSlot8 = "3:30pm";
+
+function setSupplierName() {
+  document.getElementById("supplier-name").textContent = supplier_name;
+}
