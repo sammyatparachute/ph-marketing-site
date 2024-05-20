7bd5aa5b9498394f20646afc08b803f4775b285b
sammygallo <sammy.gallo@parachutehealth.com>
2024-05-20 12:36:11 -0400

    Added Alpine Home Medical

--- suppliers.js
+++ suppliers.js
@@ -1,5 +1,19 @@
 const suppliers = [
   {
+    id: 372,
+    name: "Alpine Home Medical",
+    external_id: "FVZ-ICYR-UV6C-QM",
+    url: "alpinehomemedical",
+    logo: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/5685ffdf-2a73-46d4-a4a9-d84886053535/Alpine+Home+Medical.png?content-type=image%2Fpng",
+    description: null,
+    headline: "A better way to order medical equipment",
+    service_area: ["ID", "UT"],
+    go_live_date: "2024-05-10T04:00:00.000Z",
+    active: true,
+    created_at: "2024-05-20T16:35:22.271Z",
+    updated_at: null,
+  },
+  {
     id: 371,
     name: "Copper Star Home Medical Supplies",
     external_id: "Y5YSZ-2Z20N-HQIL",
