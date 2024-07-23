2b8f6989fa95260238b0a6c214fa1a40e24cbd98
sammygallo <sammy.gallo@parachutehealth.com>
2024-07-23 14:24:32 -0400

    Added One Step Ahead & Home Medical Equipment

--- suppliers.js
+++ suppliers.js
@@ -1,11 +1,27 @@
 const suppliers = [
   {
+    id: 393,
+    name: "One Step Ahead & Home Medical Equipment",
+    external_id: "QRCM8-RA-LE-LIDC",
+    url: "one-step-ahead-home-medical-equipment",
+    logo: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/c336cb1c-85b3-42a8-8768-80e3f3cf249d/One+Step+Ahead+%26+Home+Medical+Equipment.png?content-type=image%2Fpng",
+    description:
+      "One Step Ahead & HME has everything you need in-home medical equipment. We carry a full line of durable medical equipment. Our customers' health and comfort is our top priority. We are here because we care. We want our customers to know that when they come to either of our locations, they will receive compassionate and friendly service with a personal touch.",
+    headline: "A better way to order medical equipment",
+    service_area: ["IL", "IN"],
+    go_live_date: "2024-07-19T04:00:00.000Z",
+    active: true,
+    created_at: "2024-07-23T18:23:48.009Z",
+    updated_at: null,
+  },
+  {
     id: 392,
     name: "Commonwealth Home Health Care",
     external_id: "64-FS59F-RYM0-NT",
     url: "commonwealthhhc",
     logo: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/965e8bf5-84c2-4e9a-b9ef-1a55ae0ab404/Commonwealth+Home+Health+Care.png?content-type=image%2Fpng",
-    description: "Commonwealth Home Health Care (CHHC) is a local, Joint Commission accredited medical equipment company with the goal of providing the very best service and products to aid patient care at home. Our services and products cover all aspects of healthcare including Medical Equipment, Home Oxygen, Safety and Rehabilitation Products. We also provide Respiratory and Medical Equipment consultations.",
+    description:
+      "Commonwealth Home Health Care (CHHC) is a local, Joint Commission accredited medical equipment company with the goal of providing the very best service and products to aid patient care at home. Our services and products cover all aspects of healthcare including Medical Equipment, Home Oxygen, Safety and Rehabilitation Products. We also provide Respiratory and Medical Equipment consultations.",
     headline: "A better way to order medical equipment",
     service_area: ["VA", "NC"],
     go_live_date: "2024-07-18T04:00:00.000Z",
