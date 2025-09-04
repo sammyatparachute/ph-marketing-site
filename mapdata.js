e127715feae206be2e3a4530d40b3d86df51a698
sammyatparachute <48567572+sammyatparachute@users.noreply.github.com>
2025-09-04 18:05:09 -0400

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,879 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State Description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Add location markers using latitude and longitude!",
+    location_color: "#2041D4",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "/static/lib/simplemaps/map_images/",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      hide: "yes",
+      color: "#b167d3",
+      hover_color: "#520079"
+    },
+    AK: {
+      name: "Alaska",
+      hide: "yes",
+      hover_color: "#520079",
+      color: "#b167d3"
+    },
+    FL: {
+      name: "Florida",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    NH: {
+      name: "New Hampshire",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    VT: {
+      name: "Vermont",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    ME: {
+      name: "Maine",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    RI: {
+      name: "Rhode Island",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    NY: {
+      name: "New York",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    PA: {
+      name: "Pennsylvania",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    NJ: {
+      name: "New Jersey",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    DE: {
+      name: "Delaware",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    MD: {
+      name: "Maryland",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    VA: {
+      name: "Virginia",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    WV: {
+      name: "West Virginia",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    OH: {
+      name: "Ohio",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    IN: {
+      name: "Indiana",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    IL: {
+      name: "Illinois",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    CT: {
+      name: "Connecticut",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    WI: {
+      name: "Wisconsin",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    NC: {
+      name: "North Carolina",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    DC: {
+      name: "District of Columbia",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    MA: {
+      name: "Massachusetts",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    TN: {
+      name: "Tennessee",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    AR: {
+      name: "Arkansas",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    MO: {
+      name: "Missouri",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    GA: {
+      name: "Georgia",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    SC: {
+      name: "South Carolina",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    KY: {
+      name: "Kentucky",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    AL: {
+      name: "Alabama",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    LA: {
+      name: "Louisiana",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    MS: {
+      name: "Mississippi",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    IA: {
+      name: "Iowa",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    MN: {
+      name: "Minnesota",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    OK: {
+      name: "Oklahoma",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    TX: {
+      name: "Texas",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    NM: {
+      name: "New Mexico",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    KS: {
+      name: "Kansas",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    NE: {
+      name: "Nebraska",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    SD: {
+      name: "South Dakota",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    ND: {
+      name: "North Dakota",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    WY: {
+      name: "Wyoming",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    MT: {
+      name: "Montana",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    CO: {
+      name: "Colorado",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    UT: {
+      name: "Utah",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    AZ: {
+      name: "Arizona",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    NV: {
+      name: "Nevada",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    OR: {
+      name: "Oregon",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    WA: {
+      name: "Washington",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    CA: {
+      name: "California",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    MI: {
+      name: "Michigan",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    ID: {
+      name: "Idaho",
+      color: "#b167d3",
+      hover_color: "#520079",
+      inactive: "yes"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes",
+      color: "#b167d3",
+      hover_color: "#520079"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes",
+      color: "#b167d3",
+      hover_color: "#520079"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes",
+      color: "#b167d3",
+      hover_color: "#520079"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      color: "#b167d3",
+      hover_color: "#520079"
+    },
+    AS: {
+      name: "American Samoa",
+      hide: "yes",
+      color: "#b167d3",
+      hover_color: "#520079"
+    }
+  },
+  locations: {
+    "0": {
+      name: "Averi Thompson",
+      lat: "36.859618",
+      lng: "-76.036212",
+      opacity: "1",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Virginia Beach, VA"
+    },
+    "1": {
+      name: "Bethany Visser",
+      lat: "30.446540",
+      lng: "-87.207598",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Pensacola, FL"
+    },
+    "2": {
+      name: "Blake Townsend",
+      lat: "40.763258",
+      lng: "-73.980913",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "New York, NY"
+    },
+    "3": {
+      name: "Greg Weyl",
+      lat: "39.748820",
+      lng: "-104.976302",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Denver, CO"
+    },
+    "4": {
+      name: "Jordain Buckland",
+      lat: "33.502031",
+      lng: "-112.087773",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Phoenix, AZ"
+    },
+    "5": {
+      name: "John Ciaccio",
+      lat: "36.197745",
+      lng: "-86.767169",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Nashville,TN"
+    },
+    "6": {
+      name: "James Sullivan",
+      lat: "30.291861",
+      lng: "-97.745345",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Austin, TX"
+    },
+    "7": {
+      name: "Halee Escamilla",
+      lat: "32.791271",
+      lng: "-96.779489",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Dallas,TX"
+    },
+    "8": {
+      name: "Saskia Ottley",
+      lat: "25.790737",
+      lng: "-80.219904",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Miami,FL"
+    },
+    "9": {
+      name: "Ben Reeve",
+      lat: "44.589382",
+      lng: "-89.660095",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Wisconsin"
+    },
+    "10": {
+      name: "Hayden Hoffman",
+      lat: "39.896484",
+      lng: "-83.013256",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Ohio"
+    },
+    "11": {
+      name: "Eddy Roth",
+      lat: "39.419354",
+      lng: "-76.791680",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Owings mills, MD"
+    },
+    "12": {
+      name: "Gary Mosely",
+      lat: "33.734162",
+      lng: "-84.399622",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Atlanta,GA"
+    },
+    "13": {
+      name: "Brian Osorio",
+      lat: "40.747157",
+      lng: "-111.885242",
+      type: "image",
+      image_url: "https://images.squarespace-cdn.com/content/5df13db27cfbe70b38ae20dd/8d3bb82f-e9e9-4a45-811f-65af120bd25d/Logo-purple-green.png?content-type=image%2Fpng",
+      description: "Salt Lake City, UT"
+    }
+  },
+  labels: {
+    "0": {
+      display: "no"
+    },
+    NH: {
+      x: "932",
+      y: "183",
+      pill: "no",
+      width: 45,
+      display: "no"
+    },
+    VT: {
+      x: "883",
+      y: "243",
+      pill: "no",
+      width: 45,
+      display: "no"
+    },
+    RI: {
+      x: "932",
+      y: "273",
+      pill: "no",
+      width: 45,
+      display: "no"
+    },
+    NJ: {
+      x: "883",
+      y: "273",
+      pill: "no",
+      width: 45,
+      display: "no"
+    },
+    DE: {
+      x: "883",
+      y: "303",
+      pill: "no",
+      width: 45,
+      display: "no"
+    },
+    MD: {
+      x: "932",
+      y: "303",
+      pill: "no",
+      width: 45,
+      display: "no"
+    },
+    DC: {
+      x: "884",
+      y: "332",
+      pill: "no",
+      width: 45,
+      display: "no"
+    },
+    MA: {
+      x: "932",
+      y: "213",
+      pill: "no",
+      width: 45,
+      display: "no"
+    },
+    CT: {
+      x: "932",
+      y: "243",
+      pill: "no",
+      width: 45,
+      display: "no"
+    },
+    HI: {
+      x: 305,
+      y: 565,
+      pill: "no",
+      display: "no"
+    },
+    AK: {
+      x: "113",
+      y: "495",
+      display: "no",
+      pill: "no"
+    },
+    FL: {
+      x: "773",
+      y: "510",
+      display: "no",
+      pill: "no"
+    },
+    ME: {
+      x: "893",
+      y: "85",
+      display: "no",
+      pill: "no"
+    },
+    NY: {
+      x: "815",
+      y: "158",
+      display: "no",
+      pill: "no"
+    },
+    PA: {
+      x: "786",
+      y: "210",
+      display: "no",
+      pill: "no"
+    },
+    VA: {
+      x: "790",
+      y: "282",
+      display: "no",
+      pill: "no"
+    },
+    WV: {
+      x: "744",
+      y: "270",
+      display: "no",
+      pill: "no"
+    },
+    OH: {
+      x: "700",
+      y: "240",
+      display: "no",
+      pill: "no"
+    },
+    IN: {
+      x: "650",
+      y: "250",
+      display: "no",
+      pill: "no"
+    },
+    IL: {
+      x: "600",
+      y: "250",
+      display: "no",
+      pill: "no"
+    },
+    WI: {
+      x: "575",
+      y: "155",
+      display: "no",
+      pill: "no"
+    },
+    NC: {
+      x: "784",
+      y: "326",
+      display: "no",
+      pill: "no"
+    },
+    TN: {
+      x: "655",
+      y: "340",
+      display: "no",
+      pill: "no"
+    },
+    AR: {
+      x: "548",
+      y: "368",
+      display: "no",
+      pill: "no"
+    },
+    MO: {
+      x: "548",
+      y: "293",
+      display: "no",
+      pill: "no"
+    },
+    GA: {
+      x: "718",
+      y: "405",
+      display: "no",
+      pill: "no"
+    },
+    SC: {
+      x: "760",
+      y: "371",
+      display: "no",
+      pill: "no"
+    },
+    KY: {
+      x: "680",
+      y: "300",
+      display: "no",
+      pill: "no"
+    },
+    AL: {
+      x: "655",
+      y: "405",
+      display: "no",
+      pill: "no"
+    },
+    LA: {
+      x: "550",
+      y: "435",
+      display: "no",
+      pill: "no"
+    },
+    MS: {
+      x: "600",
+      y: "405",
+      display: "no",
+      pill: "no"
+    },
+    IA: {
+      x: "525",
+      y: "210",
+      display: "no",
+      pill: "no"
+    },
+    MN: {
+      x: "506",
+      y: "124",
+      display: "no",
+      pill: "no"
+    },
+    OK: {
+      x: "460",
+      y: "360",
+      display: "no",
+      pill: "no"
+    },
+    TX: {
+      x: "425",
+      y: "435",
+      display: "no",
+      pill: "no"
+    },
+    NM: {
+      x: "305",
+      y: "365",
+      display: "no",
+      pill: "no"
+    },
+    KS: {
+      x: "445",
+      y: "290",
+      display: "no",
+      pill: "no"
+    },
+    NE: {
+      x: "420",
+      y: "225",
+      display: "no",
+      pill: "no"
+    },
+    SD: {
+      x: "413",
+      y: "160",
+      display: "no",
+      pill: "no"
+    },
+    ND: {
+      x: "416",
+      y: "96",
+      display: "no",
+      pill: "no"
+    },
+    WY: {
+      x: "300",
+      y: "180",
+      display: "no",
+      pill: "no"
+    },
+    MT: {
+      x: "280",
+      y: "95",
+      display: "no",
+      pill: "no"
+    },
+    CO: {
+      x: "320",
+      y: "275",
+      display: "no",
+      pill: "no"
+    },
+    UT: {
+      x: "223",
+      y: "260",
+      display: "no",
+      pill: "no"
+    },
+    AZ: {
+      x: "205",
+      y: "360",
+      display: "no",
+      pill: "no"
+    },
+    NV: {
+      x: "140",
+      y: "235",
+      display: "no",
+      pill: "no"
+    },
+    OR: {
+      x: "100",
+      y: "120",
+      display: "no",
+      pill: "no"
+    },
+    WA: {
+      x: "130",
+      y: "55",
+      display: "no",
+      pill: "no"
+    },
+    ID: {
+      x: "200",
+      y: "150",
+      display: "no",
+      pill: "no"
+    },
+    CA: {
+      x: "79",
+      y: "285",
+      display: "no",
+      pill: "no"
+    },
+    MI: {
+      x: "663",
+      y: "185",
+      display: "no",
+      pill: "no"
+    },
+    PR: {
+      x: "620",
+      y: "545",
+      display: "no",
+      pill: "no"
+    },
+    GU: {
+      x: "550",
+      y: "540",
+      display: "no",
+      pill: "no"
+    },
+    VI: {
+      x: "680",
+      y: "519",
+      display: "no",
+      pill: "no"
+    },
+    MP: {
+      x: "570",
+      y: "575",
+      display: "no",
+      pill: "no"
+    },
+    AS: {
+      x: "665",
+      y: "580",
+      display: "no",
+      pill: "no"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
