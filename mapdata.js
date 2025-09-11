9e25f839d9584ce7cc4dd14d31c71eb6140f0a82
“Sammy <sammy.gallo@parachutehealth.com>
2025-09-11 14:34:10 -0400

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -8,7 +8,7 @@ var simplemaps_usmap_mapdata = {
     popups: "detect",
 
     //State defaults
-    state_description: "State Description",
+    state_description: "",//"State Description",
     state_color: "#88A4BC",
     state_hover_color: "#3B729F",
     state_url: "",
